e4c96eaea2506258858de0388b7dd67602cebcdf
Aaron Merino <aaron.merino@mail.utoronto.ca>
2018-05-16 04:12:49 -0700

    added function prototypes. might change how we represent matrices

---
+++ operations.h
@@ -0,0 +1,33 @@
+/*
+ * Operations for matrices
+ */
+
+#ifndef __OPERATIONS__H
+#define __OPERATIONS__H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MAX_MATRIX_ROWS			10
+#define MAX_MATRIX_COLLUMNS		10
+
+typedef struct Matrix {
+	int num_rows;
+	int num_collumns;
+	int **rows_ptr;
+} Matrix;
+
+
+extern Matrix add(Matrix a, Matrix b);
+
+extern Matrix dot_product(Matrix a, Matrix b);
+
+extern Matrix transpose(Matrix a);
+
+extern Matrix row_echelon_form(Matrix a);
+
+extern Matrix reduced_row_echelon_form(Matrix a);
+
+
+#endif // __OPERATIONS__H
